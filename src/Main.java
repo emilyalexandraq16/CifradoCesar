e634e509cbffa56d5fe0049a85e0f5d563aa5ab5
Kat-01\Emily <emilyalexandraq16@gmail.com>
2026-03-14 17:25:16 +0100

    Prueba para verificar que el codigo funciona con exito

--- src/Main.java
+++ src/Main.java
@@ -9,30 +9,37 @@ public class Main {
             System.out.print("Introduzca numero de desplazamiento(-25 a 25): ");
             desplazamiento = sc.nextInt();
             if (desplazamiento>25 || desplazamiento<-25) {
-                System.out.println("Error, el desplazamiento debe ser entre 25 y -25.")
+                System.out.println("Error, el desplazamiento debe ser entre 25 y -25.");
             }
         } while (desplazamiento>25|| desplazamiento<-25);
+        sc.nextLine();
+
         File fEntrada;
         do{
-            System.out.print("Introduzca el nombre del fichero de entrada");
+            System.out.print("Introduzca el nombre del fichero de entrada: ");
             String nombre= sc.nextLine();
             fEntrada= new File("src/ficheros/" + nombre);
-            if(fEntrada.exists()) {
-                System.out.print("Error, no existe.")
+
+            if(!fEntrada.exists()||!fEntrada.isFile()) {
+                System.out.println("Error, no existe.");
             }
-        } while(!fEntrada.exists());
+        } while(!fEntrada.exists()||!fEntrada.isFile());
 
         File fSalida;
         do{
-            System.out.print("Introduce la ruta ABSOLUTA del fichero de salida");
+            System.out.print("Introduce la ruta ABSOLUTA del fichero de salida: ");
             fSalida= new File(sc.nextLine());
+
             if(fSalida.exists()){
-                System.out.println("Error, el fichero ya existe.")
+                System.out.println("Error, el fichero ya existe.");
             }
         }while(fSalida.exists());
 
         try{
-            CifradoCesar.procesar
-        } catch();
+            CifradoCesar.Procesar(fEntrada.getPath(), fSalida.getAbsolutePath(), desplazamiento);
+            System.out.println("Proceso realizado con exito.");
+        } catch(Exception e){
+            System.out.println("Error" + e.getMessage());
+        }
     }
 }
